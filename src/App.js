22b924369e874a666ecf4fc88ca43db02a85741f
Lili Storm <storm.aubrey@gmail.com>
2021-04-29 22:51:44 +0200

    Initial implementation of dog component. Also looping over all dogs in the json file.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+
+function DogComponent(props) {
+  const dog = props.Dog;
+  const divstyle = {
+    border: "solid",
+    borderColor: "lightgrey",
+    borderWidth: "1px",
+    marginBottom: "5px"
+  };
+  const picstyle = {
+    borderRadius: "50%",
+    width: "120px",
+    height: "120px",
+    objectFit: "cover"
+  };
+
   return (
-    <div className="App">
+    <div style={divstyle}>
+      <h2><img style={picstyle} src={dog.img} alt=""/> {dog.name}</h2>
+      <p>{dog.sex} , {dog.age} years old</p>
+    </div>
+  );
+}
+
+function App() {
+  const [cachedDogs, setData] = useState([]);
+  const getData=()=>{
+    fetch('https://api.jsonbin.io/b/6087d9c3f6655022c46d0b41')
+      .then(response => response.json())
+      .then(fetchedDogs => setData(fetchedDogs));
+  }
+  useEffect(()=>{
+    getData()
+  },[]);
+
+  return (
+    /* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +52,13 @@ function App() {
           Learn React
         </a>
       </header>
+    </div> */
+    <div>
+      {cachedDogs.map(dog =>
+        <DogComponent Dog = {dog}></DogComponent>
+      )}
     </div>
+
   );
 }
 
